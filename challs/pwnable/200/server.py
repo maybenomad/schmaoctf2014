bf89cc26eacf953faf6f57f3a6af31309c52f970
Rocco Nicosia <git@rocco.io>
2014-01-31 16:10:30 -0500

    update pwnable 200

--- challs/pwnable/200/server.py
+++ challs/pwnable/200/server.py
@@ -3,11 +3,17 @@ import subprocess
 import random
 import string
 
+BANNER = ("""Welcome to NOTEBOX, the PREMIERE note saving service!
+Use `help` to see options.\n\n""")
+
+HELP = ("""list - list all previous notes!
+new <data> - create a new note!
+read <filename> - read a note (filename of format note_XXXXXXXX)\n""")
+
 def random_string():
     return ''.join(random.sample(string.hexdigits, 8))
 
-def run_command(cmd):
-    print cmd
+def shell_exec(cmd):
     return subprocess.check_output(cmd, shell=True)
 
 class Server(SocketServer.ForkingMixIn, 
@@ -19,14 +25,16 @@ class Handler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         shell = True
-        while shell:
-            COMMANDS = ["read", "write", "list", "help"]
+        self.request.send(BANNER)
 
-            self.request.sendall('$ ')
-            data = self.request.recv(1024).split(' ')
+        while shell:
+            COMMANDS = ["read", "new", "list", "help"]
+
+            self.request.sendall('NOTEBOX> ')
+            data = self.request.recv(1024).strip().split(' ')
 
             cmd = data[0].strip()
-            args = map(lambda x: x.strip(), data[1:])
+            args = data[1:]
 
             print data[0]
             if cmd not in COMMANDS:
@@ -34,27 +42,24 @@ class Handler(SocketServer.BaseRequestHandler):
                 exit(0)
 
             if cmd == "read":
-                if len(args) != 1:
+                if len(args) != 1 or len(args[0]) != 13:
                     self.request.send("You must provide a valid filename!\n")
                     continue
-                args[0] = args[0].strip()
-                if len(args[0]) != 16:
-                    self.request.send("You must provide a valid filename!\n")
-                    continue
-                filename = "messages/" + args[0]
-                self.request.send(run_command("/bin/cat %s" % filename))
-            elif cmd == "write":
+                filename = "notes/" + args[0]
+                output = shell_exec("/bin/cat %s" % filename)
+                self.request.send(output)
+            elif cmd == "new":
                 msg = ' '.join(args)
-                filename = "messages/%s%s" % ("message_", random_string())
-                self.request.send(run_command("/bin/echo \"%s\" > %s" % (msg, filename)))
-                self.request.send("Message written to %s!\n" % filename)
+                filename = "notes/note_" + random_string()
+                output = shell_exec("/bin/echo \"%s\" > %s" % (msg, filename))
+                self.request.send(output)
+                self.request.send("Note written to %s!\n" % filename)
             elif cmd == "list": 
-                self.request.send(run_command("/bin/ls messages"))
+                output = shell_exec("/bin/ls notes")
+                self.request.send(output)
             elif cmd == "help":
-                self.request.send("""
-                list - list all previously deposited messages!\n
-                write <data> - create a new message!\n
-                read <filename> - read a message (filename of format message_XXXXXXXX)!\n""")
+                self.request.send(HELP)
+
 
 def drop_privs(gid, uid):
     if os.getuid() == 0:
@@ -67,5 +72,5 @@ if __name__ == '__main__':
 
     server = Server((HOST, PORT), Handler)
 
-    #drop_privs()
+    # drop_privs()
     server.serve_forever()
